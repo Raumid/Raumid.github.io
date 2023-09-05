3613e6e57c7f65cca1ee822063db89863486d2fc
rafselem <101129751+rafselem@users.noreply.github.com>
2023-09-05 17:54:33 -0600

    .

--- CV/assets/js/index.js
+++ CV/assets/js/index.js
@@ -8,9 +8,17 @@ btnDownload.addEventListener("click", async function() {
   window.jsPDF = window.jspdf.jsPDF;
 
   const doc = new jsPDF();
+  // doc.addPage("a4", "landscape");
 
   //TRY WITH IMG CANVAS
-  const contentCanvas = await html2canvas(document.getElementById("contenido"));
+  const contentCanvas = await html2canvas(document.getElementById("contenido")
+  ,{
+  //   scrollX:0,
+  //   scrollY:0,
+    windowWidth: 2000,
+    windowHeight: 4000
+  }
+  );
 
   const imgWidth = contentCanvas.width;
   const imgHeight = contentCanvas.height;
