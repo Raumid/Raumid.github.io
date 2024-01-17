d1ae30089b12a3fcf99554b85b86ec97e3b5832a
Raumid <raumid98@gmail.com>
2024-01-17 17:53:55 -0600

    .

---
+++ invitacionBoda/assets/js/music.js
@@ -0,0 +1,32 @@
+const btnAudio = document.getElementById('btn-audio');
+const musicPlayer = document.getElementById('musicPlayer');
+const icon = document.getElementById('iconMusicPlayer');
+
+document.addEventListener('DOMContentLoaded', ($event) => {
+    musicPlayer.play();
+});
+
+
+btnAudio.addEventListener('click', () => {
+    handlerMusicPlayer();
+});
+
+musicPlayer.addEventListener('ended', () => {
+    musicPlayer.currentTime = 0;
+    musicPlayer.play();
+});
+
+function handlerMusicPlayer() {
+    icon.className = '';
+    if(musicPlayer.paused) 
+    {
+        musicPlayer.play();
+        icon.classList.add('fa', 'fa-pause')    
+    }
+    else 
+    {
+        musicPlayer.pause();
+        icon.classList.add('fa', 'fa-play')
+    }
+}
+
